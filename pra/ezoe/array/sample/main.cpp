7f5665d5d4dff9d502bd41e9a9cac6a77876c8b1
hyoshie_vm <dymn9.term@gmail.com>
2022-08-18 13:32:36 +0900

    間接参照演算子の確認

--- pra/ezoe/array/sample/main.cpp
+++ pra/ezoe/array/sample/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <mymacro>
 
-int main() {
+void printIter() {
   std::array<int, 5> a = {1, 2, 3, 4, 5};
   auto first = std::begin(a);
   auto last = std::end(a);
@@ -11,4 +11,19 @@ int main() {
   PRINT(*iter);
   ++iter;
   PRINT(*iter);
+}
+
+void useIndirectionOperator() {
+  std::array<int, 5> a = {1, 2, 3, 4, 5};
+  auto iter = a.begin();
+  int x = *iter;
+  *iter = 0;
+  PRINT(x);
+  PRINT(*iter);
+}
+
+int main() {
+  // printIter();
+  useIndirectionOperator();
+  ;
 }
