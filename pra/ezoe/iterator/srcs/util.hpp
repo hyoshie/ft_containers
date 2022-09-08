246acef9b9bd0c725736befe7d60face34aaed37
hyoshie_vm <dymn9.term@gmail.com>
2022-09-08 17:55:12 +0900

    compileできるようにクラス順修正

--- pra/ezoe/iterator/srcs/util.hpp
+++ pra/ezoe/iterator/srcs/util.hpp
@@ -30,9 +30,22 @@ struct enable_if<true, T> {
 // ----------------------------------------------------
 // is_integral
 // ----------------------------------------------------
-template <typename T>
-struct is_integral : public is_integral_helper<remove_cv<T>>::type {};
 
+// integral_constant
+template <typename T, T v>
+struct integral_constant {
+  static const T value = v;
+  typedef T value_type;
+  typedef integral_constant<T, v> type;
+  const value_type operator()() const { return value; }
+};
+
+// The type used as a compile-time boolean with true value.
+typedef integral_constant<bool, true> true_type;
+
+/// The type used as a compile-time boolean with false value.
+
+typedef integral_constant<bool, false> false_type;
 // is_integral_helper
 template <typename>
 struct is_integral_helper : public false_type {};
@@ -76,21 +89,6 @@ struct is_integral_helper<long long> : public true_type {};
 template <>
 struct is_integral_helper<unsigned long long> : public true_type {};
 
-// integral_constant
-template <typename T, T v>
-struct integral_constant {
-  static const T value = v;
-  typedef T value_type;
-  typedef integral_constant<T, v> type;
-  const value_type operator()() const { return value; }
-};
-
-// The type used as a compile-time boolean with true value.
-typedef true_type integral_constant<bool, true>;
-
-/// The type used as a compile-time boolean with false value.
-typedef false_type integral_constant<bool, false>;
-
 // remove_cv
 template <class T>
 struct remove_cv {
@@ -111,6 +109,39 @@ template <class T>
 struct remove_cv<const volatile T> {
   typedef T type;
 };
+
+template <typename T>
+struct is_integral
+    : public is_integral_helper<typename remove_cv<T>::type>::type {};
+
+// ----------------------------------------------------
+// equal
+// ----------------------------------------------------
+
+// template <class InputIt1, class InputIt2>
+// bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
+//   for (; first1 != last1; ++first1, ++first2) {
+//     if (!(*first1 == *first2)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// template <class InputIt1, class InputIt2, class BinaryPredicate>
+// bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+//            BinaryPredicate p) {
+//   for (; first1 != last1; ++first1, ++first2) {
+//     if (!p(*first1, *first2)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// ----------------------------------------------------
+// lexicographical_compare
+// ----------------------------------------------------
 
 };  // namespace ft
 
